207e6a6135c982e98e7633b740349cf11b7cefe7
Skelmis <ethan.mckee-harris@bastionsecurity.co.nz>
2024-05-10 18:52:06 +1200

    feat: initial site

---
+++ app.py
@@ -0,0 +1,55 @@
+import os
+import secrets
+import subprocess
+from pathlib import Path
+
+from flask import Flask, request, flash, redirect, url_for
+
+UPLOAD_FOLDER = "./uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "zip"}
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secrets.token_urlsafe(4) + "." + file.filename.rsplit(".", 1)[1]
+            zip_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(zip_path)
+
+            if filename.endswith(".zip"):
+                # Unzip the files and delete the zip itself
+                subprocess.run(["unzip", zip_path, "-d", UPLOAD_FOLDER])
+                Path(zip_path).unlink()
+
+            return redirect(url_for("upload_file", name=filename))
+
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    """
+
+
+if __name__ == "__main__":
+    app.run()
